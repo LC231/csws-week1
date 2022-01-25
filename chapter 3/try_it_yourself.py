e5f5bcce37c1b470ae53bc0a3155a00e576a0a1b
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-01-25 13:44:56 +0000

    task 2 done

--- chapter 3/try_it_yourself.py
+++ chapter 3/try_it_yourself.py
@@ -1,6 +1,11 @@
-names = ['H','L','W','G','A']
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
-print(names[4])
+names = ['H','E','L','L','O']
+message = f"hello {names[0]}"
+print(message)
+message = f"hello {names[1]}"
+print(message)
+message = f"hello {names[2]}"
+print(message)
+message = f"hello {names[3]}"
+print(message)
+message = f"hello {names[4]}"
+print(message)
