f329cc2e00c6070e3537aa30a4fd050ebb00456c
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-01-25 13:22:56 +0000

    folder for chapter 2

---
+++ chapter_2/try_it_yourself.py
@@ -0,0 +1,5 @@
+name = "Nelson Mandela"
+quote = "The greatest glory in living lies not in never falling, but in rising every time we fall."
+message = f"{name} once said,{quote}"
+print (message)
+
