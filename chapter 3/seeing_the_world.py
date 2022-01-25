e046bb4a9eb08397189ad4e84c5857c9b8c23b4e
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-01-25 14:32:50 +0000

    3.8 complete

---
+++ chapter 3/seeing_the_world.py
@@ -0,0 +1,14 @@
+locations = ['Iceland','Japan','France','Greece','South Africa']
+print(locations)
+print(sorted(locations))
+print(locations)
+
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
