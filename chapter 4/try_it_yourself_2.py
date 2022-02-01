7da2b57e3314dd1fd0082ca471d5995d7b88ce84
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-01 13:57:07 +0000

    4.10 done

---
+++ chapter 4/try_it_yourself_2.py
@@ -0,0 +1,4 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(f"the first three items in the list are: {players[:3]}")
+print(f"three items from the middle of the list are: {players[1:4]}")
+print(f"the last three items of the lsit are: {players[-3:]}")
