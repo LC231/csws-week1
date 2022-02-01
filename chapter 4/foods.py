de478f712979b8f247b7002d5771fa49c1bb5628
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-01 13:47:35 +0000

    commented

--- chapter 4/foods.py
+++ chapter 4/foods.py
@@ -1,6 +1,9 @@
 my_foods = ['pizza', 'falafel', 'carrot cake']
 friend_foods = my_foods[:]# this line copies the list into a new list
 
+#this doesn't work:
+#friends_foods = my_foods
+
 my_foods.append('cannoli')
 friend_foods.append('ice cream')
 
