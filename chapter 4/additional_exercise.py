16e4cbabad570be307ec78c68831f90a4a909367
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-01 14:13:32 +0000

    task done

---
+++ chapter 4/additional_exercise.py
@@ -0,0 +1,6 @@
+letters = [chr(i) for i in range(ord('a'), ord('z')+ 1)]
+letters_s = [chr(i) for i in range(ord('b'), ord('z')+ 1,2)]
+letters_others = [chr(i) for i in range(ord('a'), ord('z')+ 1,2)]
+print(letters)
+print(letters_s)
+print(letters_others)
