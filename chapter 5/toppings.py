8e35fd39da260b861f9d2bbec0a6bd579fa9542d
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-08 13:47:27 +0000

    d

--- chapter 5/toppings.py
+++ chapter 5/toppings.py
@@ -1,9 +1,11 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                         'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
 for requested_topping in requested_toppings:
-     if requested_topping == 'green peppers':
-           print("Sorry, we are out of green peppers right now.")
+     if requested_topping in available_toppings:
+           print(f"Adding {requested_topping}.")
      else:
-        print(f"Adding {requested_topping}.")
+           print(f"Sorry, we don't have {requested_topping}.")
 
 print("\nFinished making your pizza!")
