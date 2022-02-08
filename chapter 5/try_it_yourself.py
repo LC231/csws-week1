5a5172711c6fd3c9c88979d9c64b4a58dda774bf
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-08 14:01:38 +0000

    5.9 done

--- chapter 5/try_it_yourself.py
+++ chapter 5/try_it_yourself.py
@@ -1,7 +1,10 @@
-usernames = ['name1','name2','name3','name4','admin']
+usernames = []
 
-for username in usernames:
-    if username == 'admin':
-        print("Hello admin, would you like to see a status report")
-    else:
-         print(f"Hello {username}, thank you for logging in")
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report")
+        else:
+            print(f"Hello {username}, thank you for logging in")
+else:
+    print("we need to find more users")
