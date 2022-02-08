a392667ecae78e155ea976b33e95d213a7cc556d
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-08 14:40:48 +0000

    comment

--- chapter 7/greeter.py
+++ chapter 7/greeter.py
@@ -3,3 +3,10 @@ prompt += "\nWhat is your first name? "
 
 name = input(prompt)
 print(f"\nHello, {name}!")
+
+#  >>> age = input("How old are you? ")
+#   How old are you? 21
+# >>> age = int(age)
+#   >>> age >= 18
+#  True
+# the above is for integers becasue if you dont use int() it is read as a string
