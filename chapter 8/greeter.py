dfddc6f6f330ea0b15967158301d97b87711e671
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-02-15 13:41:59 +0000

    function with a while loop

--- chapter 8/greeter.py
+++ chapter 8/greeter.py
@@ -1,5 +1,13 @@
-def greet_user(username):
-    """Display a simple greeting"""""
-    print(f"Hello,{username.title()}!")
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-greet_user('jesse')
+   # This is an infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
